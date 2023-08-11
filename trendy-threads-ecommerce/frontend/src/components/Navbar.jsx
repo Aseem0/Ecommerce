4650bca620f12168517cf6e68bd5ba2ed77faf4d
Aseem rai <raiaseem25@gmail.com>
2023-08-11 17:36:46 +0545

    feat:add .

--- trendy-threads-ecommerce/frontend/src/components/Navbar.jsx
+++ trendy-threads-ecommerce/frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,12 +7,39 @@ const NavbarComp = () => {
   const user = useSelector((state) => state.auth);
   return (
     <header>
-      <Navbar bg="dark" variant="dark"></Navbar>;
-      <Container>
-        <Link to="/home">
-          <Navbar.Brand>Trendy thrifts</Navbar.Brand>
-        </Link>
-      </Container>
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+        <Container>
+          <Link to="{/home}">
+            <Navbar.Brand>Thrift Haven</Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to={""}>
+                <i className="fas fa-shopping-cart mt-3 be-2"></i>
+              </Link>
+              {user.isLoggedIn ? (
+                <NavDropdown title={user.name} id="usernmae">
+                  <Link to={"/profile"}>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </Link>
+                  <Link to={"/"}>
+                    <NavDropdown.Item>Logout</NavDropdown.Item>
+                  </Link>
+                </NavDropdown>
+              ) : (
+                <>
+                  <Link>
+                    <Nav.Link>
+                      <i className="fas fa-user"></i> Sign In
+                    </Nav.Link>
+                  </Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
--- trendy-threads-ecommerce/frontend/src/pages/Home.jsx
+++ trendy-threads-ecommerce/frontend/src/pages/Home.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const getProducts = async () => {
+    try {
+      const product = await axios.get(
+        import.meta.env.VITE_SERVER_URL + "/api/v1/product"
+      );
+
+      console.log(product);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return <div>Home</div>;
 };
 
